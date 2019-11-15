d3ab62514690570a0bf27495817bd13d0a57f886
Maisarasid <55817763+Maisarasid@users.noreply.github.com>
2019-11-16 07:14:05 +0800

    Update Homepage.js

--- javascript/Homepage.js
+++ javascript/Homepage.js
@@ -16,23 +16,4 @@ buss_next.addEventListener('click', function () {
 
 })
 
-// Slick slider init
 
-$(".sliderConf").slick({
-
-    autoplay: true,
-    dots: true,
-    customPaging : function(slider, i) {
-        var thumb = $(slider.$slides[i]).data('thumb');
-        return '<a><img src="'+thumb+'"></a>';
-    },
-
-    responsive: [{
-        breakpoint: 500,
-        settings: {
-            dots: false,
-            arrows: false,
-            infinite: false,
-            slidesToShow: 2,
-            slidesToScroll: 2
-        }
