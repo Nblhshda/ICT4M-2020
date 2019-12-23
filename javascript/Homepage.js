7fb12fe07a5f9509ff2cd797be7eda769ce4ff19
Maisarasid <55817763+Maisarasid@users.noreply.github.com>
2019-12-23 23:09:34 +0800

    Update Homepage.js

--- javascript/Homepage.js
+++ javascript/Homepage.js
@@ -16,4 +16,28 @@ buss_next.addEventListener('click', function () {
 
 })
 
+var modal01 = document.getElementById("myModal01");
+
+// Get the button that opens the modal
+var submit = document.getElementById("submit");
+
+// Get the <span> element that closes the modal
+var span01 = document.getElementsByClassName("close01")[0];
+
+// When the user clicks on the button, open the modal
+btn01.onclick = function() {
+  modal01.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span01.onclick = function() {
+  modal01.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal01) {
+    modal01.style.display = "none";
+  }
+}
 
